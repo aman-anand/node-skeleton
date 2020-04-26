dba36b6814a7a002c3edf6c51aa5cfd674147c73
Aman Anand <aman.anandx3@gmail.com>
2020-04-27 01:22:15 +0530

    fixes

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ var app = express();
 // app.use(require('./lib/jwtHelper').middleware())
 //load db
 
-const _ = require('./lib/Database/mongoConn1').then(console.log).catch(e => {
+const _ = require('./lib/Database/mongoConn1').then(k=>{
+
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({extended: false}));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+
+    app.use(require('./main.router'));
+    app.use(require('./api/utils/errorHandler'))
+    console.log("Now accepting api calls");
+}).catch(e => {
     console.log('caught', e);
     process.exit();
 })
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(require('./main.router'));
-app.use(require('./api/utils/errorHandler'))
 module.exports = app;
